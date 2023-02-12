06f231be25884d4b11b435aa04f3035b9b8121b6
Paula Jardim <paulajardimf@gmail.com>
2023-02-12 20:11:24 -0300

    exercício finalizado

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors"
+import { productRouter } from "./routers/productRouter";
+import { brandRouter } from "./routers/brandRouter";
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(3003, () => console.log("Server rodando na porta 3003"))
+
+app.use("/products", productRouter)
+app.use("/brands", brandRouter)
+
---
+++ src/routers/brandRouter.ts
@@ -0,0 +1,4 @@
+import express, { Request, Response } from "express";
+import knex from "knex"
+
+export const brandRouter = express.Router()
